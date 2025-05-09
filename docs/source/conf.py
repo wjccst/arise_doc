b0879e4dbc158fd595ca2775bf8dd589921ebd79
wjccst <1183817213@qq.com>
2025-05-10 01:30:32 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,9 +28,9 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 #theme#
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# import sphinx_rtd_theme
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 source_suffix = {
     '.rst': 'restructuredtext',
