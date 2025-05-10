a29379173eb6e90f09c3a988e5e1960c836e209d
wjccst <1183817213@qq.com>
2025-05-10 09:51:23 +0800

    change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ html_static_path = ['_static']
 
 #theme#
 # import sphinx_rtd_theme
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 source_suffix = {
